7636e91aed3359d28172e5081d619bfc25b0682c
Alex Grover <hello@alexgrover.me>
2025-10-16 09:31:18 -0700

    Add prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,25 +1,25 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import config from "@alex-grover/eslint-config";
-import { FlatCompat } from "@eslint/eslintrc";
-import { defineConfig } from "eslint/config";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import config from '@alex-grover/eslint-config'
+import { FlatCompat } from '@eslint/eslintrc'
+import { defineConfig } from 'eslint/config'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 export default defineConfig([
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
     ],
   },
   ...config.map((entry) =>
@@ -27,9 +27,9 @@ export default defineConfig([
       ? {
           ...entry,
           plugins: Object.fromEntries(
-            Object.entries(entry.plugins).filter(([name]) => name !== "import"),
+            Object.entries(entry.plugins).filter(([name]) => name !== 'import'),
           ),
         }
       : entry,
   ),
-]);
+])
