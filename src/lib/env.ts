2f088c49fd202aa4320e39a2e5944088f29713ae
Alex Grover <hello@alexgrover.me>
2025-10-16 16:24:37 -0700

    Set up drizzle and env

---
+++ src/lib/env.ts
@@ -0,0 +1,9 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod/mini'
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().check(z.minLength(1)),
+  },
+  experimental__runtimeEnv: process.env,
+})
