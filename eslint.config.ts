cb23dd3797658168e793d5500daf91efc9708b91
Alex Grover <hello@alexgrover.me>
2025-10-16 08:39:15 -0700

    Add `@alex-grover/eslint-config`

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import config from "@alex-grover/eslint-config";
 import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from "eslint/config";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,7 +11,7 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default defineConfig([
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
@@ -20,6 +22,14 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
-];
-
-export default eslintConfig;
+  ...config.map((entry) =>
+    entry.plugins
+      ? {
+          ...entry,
+          plugins: Object.fromEntries(
+            Object.entries(entry.plugins).filter(([name]) => name !== "import"),
+          ),
+        }
+      : entry,
+  ),
+]);
